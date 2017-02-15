86edf6329fe55eeb3f3b60a74c5f6f67ac2621d0
Bruna Vasconcelos <brunapvasconcelos@gmail.com>
2017-02-14 21:10:47 -0300

    Organização do projeto
    Separação de pacotes

---
+++ src/model/bean/Aluno.java
@@ -0,0 +1,34 @@
+package model.bean;
+
+public class Aluno {
+
+	private String cpf;
+	private String nome;
+	private String curso;
+	private String dataInicio;
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getDataInicio() {
+		return dataInicio;
+	}
+	public void setDataInicio(String dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+	
+}
